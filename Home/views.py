dbae7335f6b23351dfc9b1fddf59cd57af4ad2d1
Aditya Ajay <aditya.ajay@outlook.in>
2019-09-18 21:42:17 +0530

    added templates

--- Home/views.py
+++ Home/views.py
@@ -7,8 +7,23 @@ def landing_page(request):
 
 
 def contact_form(request):
-    name = request.POST.get('Name')
-    email = request.POST.get('Email')
-    subject = request.POST.get('Subject')
-    message = request.POST.get('Message')
-    pass
+    name = request.POST.get('n')
+    email = request.POST.get('e')
+    subject = request.POST.get('sub')
+    message = request.POST.get('msg')
+    sender = 'mailautomationsdigital@gmail.com'
+    receiver = sender
+    print(name, email, subject, message)
+    message += f"\n{name}\n{email}"
+    message = 'Subject: {}\n\n{}'.format(subject, message)
+    server = smtplib.SMTP("smtp.gmail.com:587")
+    server.starttls()
+    username = 'mailautomationsdigital@gmail.com'
+    # change before deploying
+    f = open('/home/aditya/PycharmProjects/AutomationsDigital/Home/passw.txt', 'r')
+    password = f.read()
+    server.login(username, password)
+    server.sendmail(sender, receiver, message)
+    server.quit()
+    msg = "Successful"
+    return render(request, 'Home/index.html', {'msg': msg})
